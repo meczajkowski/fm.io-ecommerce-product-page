3029cd11654f90fe1644644851739812f4e58cea
Michal <michal.erik.czajkowski@gmail.com>
2023-09-22 23:45:14 +0200

    font kumbh sans

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Kumbh_Sans } from 'next/font/google';
 
 import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] });
+const kumbh = Kumbh_Sans({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={kumbh.className}>
         <Navbar />
         {children}
       </body>
