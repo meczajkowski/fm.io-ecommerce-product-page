1f579239f5d67b0db967a98d237faa7e3cda87ec
Michal <michal.erik.czajkowski@gmail.com>
2023-09-23 16:24:49 +0200

    product page - add buttons

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <main>
       <Container>
-        <section className='grid grid-cols-2  items-center px-10 py-24'>
+        <section className='grid grid-cols-2  w-full items-center justify-between px-10 py-24'>
+          {/* left side */}
           <div className=''>
             <Image
               height={450}
@@ -15,7 +16,9 @@ export default function Home() {
               className='rounded-2xl'
             />
           </div>
-          <div className=''>
+
+          {/* right side */}
+          <div className='max-w-md ml-auto mr-0'>
             <h1 className='uppercase text-primary font-bold'>
               sneaker company
             </h1>
@@ -27,6 +30,8 @@ export default function Home() {
               companion. Featuring a durable rubber outer sole, they'll
               withstand everything the weather can offer.
             </p>
+
+            {/* price section */}
             <div className='mt-7'>
               <div className='flex items-center'>
                 <h3 className='font-bold text-3xl'>$125.00</h3>
@@ -34,11 +39,28 @@ export default function Home() {
                   50%
                 </h4>
               </div>
+              <div className=''>
+                <h5 className='line-through font-bold text-muted my-2'>
+                  $250.00
+                </h5>
+              </div>
             </div>
-            <div className=''>
-              <h5 className='line-through font-bold text-muted my-2'>
-                $250.00
-              </h5>
+
+            {/* buttons */}
+            <div className='grid grid-cols-3 justify-between w-full gap-5 mt-8'>
+              {/* counter */}
+              <div className='flex items-center justify-between bg-secondary py-4 px-2 rounded-lg '>
+                <img className='cursor-pointer p-2' src='icon-minus.svg'></img>
+                <div className='font-bold'>0</div>
+                <img className='cursor-pointer p-2' src='icon-plus.svg'></img>
+              </div>
+
+              {/* add to cart */}
+              <div className='col-span-2'>
+                <button className='flex gap-3 items-center justify-center bg-primary text-white w-full py-4 rounded-lg font-bold'>
+                  <img src='icon-cart.svg' alt='' className='' /> Add to cart
+                </button>
+              </div>
             </div>
           </div>
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       colors: {
         primary: 'hsl(26, 100%, 55%)',
         'primary-muted': 'hsl(25, 100%, 94%)',
+        secondary: ' hsl(223, 64%, 98%)',
 
         muted: ' hsl(220, 14%, 75%)',
       },
